c270ab6c3d6e2a811c03983e52a6e48f36a7785a
Margarida Martelo <margmartelo@gmail.com>
2020-12-03 17:45:38 +0000

    Introducing a new unit test and passing all tests for stormy conditions

--- src/airport.js
+++ src/airport.js
@@ -13,6 +13,13 @@ class Airport{
   };
 
   clearForTakeOff(plane) {
+    if(this.isStormy()) {
+      throw new Error('cannot takeoff during storm');
+    }
     this._hangar = [];
+  }
+
+  isStormy() {
+    return false;
   }
 };
